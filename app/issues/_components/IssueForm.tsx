58b47bc392ec4fe382607ef394d8f4cbc4e59591
James Dineley <jdineley@hotmail.com>
2023-11-22 16:54:47 +0000

    Fix caching issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -58,6 +58,7 @@ const IssueForm = ({ issue }: Props) => {
       if (issue) await axios.patch("/api/issues/" + issue.id, data);
       else await axios.post("/api/issues", data);
       router.push("/issues");
+      router.refresh();
     } catch (error) {
       //   console.log("error", error);
       setSubmitting(false);
