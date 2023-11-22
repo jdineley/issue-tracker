e18e85b6d8207b63a92f8481b8ea7cd1ca11271c
James Dineley <jdineley@hotmail.com>
2023-11-22 23:42:40 +0000

    Improve the loading experience

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
-import IssueForm from "../_components/IssueForm";
+// import IssueForm from "../_components/IssueForm";
+import dynamic from "next/dynamic";
+import delay from "delay";
+import IssueFormSkeleton from "./loading";
 
-const NewIssueForm = () => {
+const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
+  ssr: false,
+  loading: () => <IssueFormSkeleton />,
+});
+
+const NewIssueForm = async () => {
   return <IssueForm />;
 };
 
